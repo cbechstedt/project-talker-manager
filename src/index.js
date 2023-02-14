9586a420f9306b5170f1d38fc3fcb7589c84a7cf
Christian <chrisnebech@hotmail.com>
2023-02-13 21:17:45 -0300

    requisito 4

--- src/index.js
+++ src/index.js
@@ -8,6 +8,30 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+function generateToken() {
+  return crypto.randomBytes(8).toString('hex');
+}
+
+const validateLogin = (req, res, next) => {
+  const { email, password } = req.body;
+  const emailRegex = /\S+@\S+\.\S+/;
+  const emailValidation = emailRegex.test(email);
+
+  if (!email) {
+    return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!emailValidation) {
+    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  return next();
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
     response.status(HTTP_OK_STATUS).send();
@@ -33,16 +57,7 @@ app.get('/talker/:id', async (req, res) => {
   return res.status(HTTP_OK_STATUS).json(talker);
 });
 
-app.post('/login', (req, res) => {
-  function generateToken() {
-    return crypto.randomBytes(8).toString('hex');
-  }
+app.post('/login', validateLogin, (req, res) => {
   const token = generateToken();
   res.status(200).json({ token });
-  
-  // const { email, password } = req.body;
-  // loginAccess = { email, password };
-  
-  // if (loginAccess) {
-  // }
 });
